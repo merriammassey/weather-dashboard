96ea7b8f6f6119fd0ee250ac34643d0b8d206d8a
Maria M <prov21_1@hotmail.com>
2021-03-10 14:55:45 -0700

    api test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,10 @@
-https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-
-3facb0dc69ce03b89e13bc8c8ec66d00
-
-// when a user types the name of a city and clicks the search button, 
-//
-
-
-// City name, state code (only for the US) and country code divided by comma. Please use ISO 3166 country codes.
-
-// validate the input 
-//https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
-// convert the city to lat long
-
-//error handling
-
-// then the city is added to the left nav bar 
-
-// and current and future conditions are updated
-// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-
-/*including an icon representation of 
-weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-
-
-
-// 
-/*
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
+var getUserRepos = function() {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&appid=3facb0dc69ce03b89e13bc8c8ec66d00").then(function(response) {
+        response.json().then(function(data) {
+          console.log(data);
+        });
+    });
+};
+    //console.log("outside");
+    
+getUserRepos();
