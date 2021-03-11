f2da929ac7e3c65fd7618aff86b07687d11e39a2
Maria M <prov21_1@hotmail.com>
2021-03-11 13:05:29 -0700

    click event

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,17 +10,47 @@
 getWeatherData();
 */
 
+$( document ).ready(function() {
 
-// on search button validate user input
+// capture form input
+var locationEl = document.querySelector("#location");
+var searchButton = document.querySelector("#location-submit");
+// add event listeners to forms
+searchButton.addEventListener("click", formSubmitHandler);
+})
+ 
+
+// on search button validate user input - attach to button click
 var getLocation = function() {
-    var location = "austin";
+    //var location = "austin";
     fetch("https://maps.googleapis.com/maps/api/geocode/json?address="+location+"&key=AIzaSyDjPtZ4p5lg5rNZd4ZphD2Sdk3OPaCKryQ").then(function(response) {
         response.json().then(function(data) {
           console.log(data);
+          //get lat long
         });
     });
 };
     //console.log("outside");
     
-getLocation();
+// a function to handle the form input
+var formSubmitHandler = function(event) {
+    console.log("button was clicked");
+    // prevent the event from bubbling up to next element
+    event.preventDefault();
+
+    // get value from input element and trim off any spaces
+    var locationEl = document.querySelector("#location");
+    var location = locationEl.value.trim();
+
+    if (location) {
+    getLocation(location);
+    //clear out the name
+    locationEl.value = "";
+    } else {
+    alert("Please enter a location");
+    }
+        console.log(location);
+ };
+
+//getLocation();
 
