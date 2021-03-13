d0604223af4ac602f847af98865c44a28a2a64ba
Maria M <prov21_1@hotmail.com>
2021-03-13 11:15:59 -0700

    local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,98 @@
 $(document).ready(function() {
-    
 
 // capture form input
-var locationEl = document.querySelector("#location");
 var searchButton = document.querySelector("#location-submit");
 
 // add event listeners to forms
 searchButton.addEventListener("click", formSubmitHandler);
 });
- 
-var getWeatherData = function(latlng) {
+
+// a function that can be called when the page loads for everything in local storage
+
+// a function that creates one button
+var makeCityButtons = function(location) {
+    //target the div to append buttons to
+    var buttonsEl = document.querySelector("#city-buttons");
+    //make city button
+    var city = document.createElement("button");
+    buttonsEl.appendChild(city);
+    //city.classList.add("col-md-3");
+    //city.classList.add("i:hover");
+    city.innerText = location;
+    console.log("make buttons");
+    //add event listener
+    city.addEventListener("click", formSubmitHandler);
+    //clear out the name
+    var locationEl = document.querySelector("#location");
+    locationEl.value = "";
+}
+
+var displayData = function(data, location) {
+    //display the date
+    //var date = luxon.DateTime.now();
+    //$("#date").text(date.toLocaleString(luxon.DateTime.DATE_SHORT));
+    //display current data
+    console.log(data.current.temp)
+    //$("#temp").text("Temperature: " + data.current.temp + "F");
+    //$("#humidity").text("Humidity: " + data.current.humidity + "%");
+    document.querySelector("#city-date").innerHTML = location + " " + luxon.DateTime.now().toLocaleString(luxon.DateTime.DATE_SHORT);
+    document.querySelector("#current-temp").innerHTML = "Temperature: " + data.current.temp + "F";
+    document.querySelector("#current-humidity").innerHTML = "Humidity: " + data.current.humidity + "%";
+    document.querySelector("#current-wind").innerHTML = "Wind Speed: " + data.current.weather.wind_speed + "mph";
+    document.querySelector("#current-uv").innerHTML = "UV Index: " + data.current.uvi;
+    // display 5 day forecast
+    document.querySelector("#day-one-date").innerHTML = luxon.DateTime.now().plus({ days: 1 }).toLocaleString(luxon.DateTime.DATE_SHORT);
+    document.querySelector("#one-temp").innerHTML = "Temperature: " + data.daily[1].temp.day+ "F";
+    document.querySelector("#one-humidity").innerHTML = "Humidity: " + data.daily[1].humidity + "%";
+
+}
+var storedCitiesArray = [];
+
+var getWeatherData = function(location, latlng) {
     // use lat long and change units to imperial
     fetch("https://api.openweathermap.org/data/2.5/onecall?"+latlng+"&appid=3facb0dc69ce03b89e13bc8c8ec66d00&units=imperial").then(function(response) {
-        response.json().then(function(data) {
+        var data = response.json().then(function(data) {
           console.log(data);
+          displayData(data, location);
+
+          //get array or create one if there is none
+        var storedCities = JSON.parse(localStorage.getItem("storedCities"));
+        //if this array has anything in it, replace storedCities array 
+        if(storedCities) {
+            storedCitiesArray = storedCities;
+        }
+        //var storedCitiesArray = localStorage.getItem("storedCities");
+          
+            //if the array doesn't include this location
+            if(!storedCitiesArray.includes(location, 0)){
+                console.log("array doesn't have this location");
+                // add this location to the array
+                console.log(typeof(storedCitiesArray));
+                console.log(storedCitiesArray);
+                storedCitiesArray.push(location);
+
+                //replace the stored array "storedCities" with one with the new location 
+                localStorage.setItem("storedCities", JSON.stringify(storedCitiesArray));
+                console.log("location is now stored");
+                // make a button for the left column
+                makeCityButtons(location);
+            //} else {
+            // if the location exists in the array in local storage, do not make a button
+            //if(localStorage.getItem(location)){
+            //    console.log("location is already stored");
+            // if the locatino is not in local storage, put it there
+            //} else {
+            //    localStorage.setItem(location, latlng);
+                
+                //and make a city button
+                // make city buttons and append them but make this replicable  
+            }
         });
     });
+
+    
+    // add event listeners to forms
+    //buttonsEl.addEventListener("submit", formSubmitHandler);
 };
     //console.log("outside");
     
@@ -30,9 +107,10 @@ var getLocation = function(location) {
           //get lat long
         var latlng = "lat="+ data.results[0].geometry.location.lat+ "&lon=" +data.results[0].geometry.location.lng;
         console.log(latlng);
-        getWeatherData(latlng);
+        getWeatherData(location, latlng);
         });
     });
+
 };
     //console.log("outside");
     
@@ -58,13 +136,11 @@ var formSubmitHandler = function(event) {
     getLocation(location);
     //console.log(latlng);
 
-    //clear out the name
-    locationEl.value = "";
+    
     } else {
     alert("Please enter a location");
-    }
-
+    
+    
  };
-
+}
 //getLocation();
-
